b5cd0eb3448c219e4ec81ce567c7c03cc3926207
PedroBonfim21 <phenrique646@gmail.com>
2025-02-27 00:24:43 -0300

    Add sign- in/up to navbar

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,19 +1,29 @@
-import { Button } from "@/components/ui/button";
+"use client"
+
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 interface AuthLayoutProps {
   children: React.ReactNode;
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) =>{
+  const pathname = usePathname();
+  const isSignIn = pathname === "/sign-in";
+
   return(
     //mudar neutral-300 to 100
     <main className="bg-neutral-300 min-h-screen">
       <div className="mx-auto max-w-screen-2xl p-4">
         <nav className="flex justify-between items-center">
           <Image src="/logo.svg" height={56} width={152} alt="logo" />
-            <Button variant="secondary">
-              Sign-up
+            <Button asChild variant="secondary">
+              <Link href={isSignIn ? "/sign-up" : "/sign-in"}>
+                {isSignIn ? "Sign Up" : "Login"}
+              </Link>
             </Button>
         </nav>
         <div className="flex flex-col items-center justify-center pt-4 md:pt-4">
