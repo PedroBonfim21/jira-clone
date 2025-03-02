d766829e6016edff9bbf324618964426a0d4a916
PedroBonfim21 <phenrique646@gmail.com>
2025-03-02 19:18:05 -0300

    disable buttons while login request is pending

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -16,7 +18,7 @@ import { useLogin } from "../api/use-login";
 
 
 export const SignInCard = () =>{
-  const { mutate } = useLogin();
+  const { mutate, isPending } = useLogin();
 
   const form = useForm<z.infer<typeof loginSchema>>({
     resolver: zodResolver(loginSchema),
@@ -75,7 +77,7 @@ export const SignInCard = () =>{
                 </FormItem>
               )}
             />
-            <Button disabled={false} size="lg" className="w-full">
+            <Button disabled={isPending} size="lg" className="w-full">
               login
             </Button>
           </form>
@@ -86,7 +88,7 @@ export const SignInCard = () =>{
       </div>
       <CardContent className="p-7 flex flex-col gap-y-4">
         <Button
-          disabled={false}
+          disabled={isPending}
           variant="secondary"
           size="lg"
           className="w-full"
@@ -95,7 +97,7 @@ export const SignInCard = () =>{
           Login with Google
         </Button>
         <Button
-          disabled={false}
+          disabled={isPending}
           variant="secondary"
           size="lg"
           className="w-full"
